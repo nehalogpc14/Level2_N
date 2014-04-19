2665536676c727845fdc41f19c9dadea55490df2
ShefaliDeosthali <shefalid360@gmail.com>
2014-04-19 16:21:48 -0700

    Working HealthBar!!!

--- HealthBar.java
+++ HealthBar.java
@@ -8,18 +8,21 @@ import java.awt.Color;
  */
 public class HealthBar extends Actor
 {
-    int health = 50;
-    int healthBarWidth = 200;
-    int healthBarHeight = 50;
+    int health = 120;
+    int totalTime = 120;
+    int healthBarWidth = 120;
+    int healthBarHeight = 75;
     int pixelsPerHealthPoint = (int)healthBarWidth/health;
+    long startTime;
     /**
      * Act - do whatever the HealthBar wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
- //   public HealthBar()
-   // {
-  //      update();
-  //  }
+    public HealthBar()
+    {
+        startTime = System.currentTimeMillis();  
+    }
+    
     public void act() 
     {
         update();
@@ -32,12 +35,16 @@ public class HealthBar extends Actor
         myImage.drawRect(0, 0, healthBarWidth + 1, healthBarHeight + 1);
         myImage.setColor(Color.GREEN);
         myImage.fillRect(1, 1, health*pixelsPerHealthPoint, healthBarHeight);
+        // Later (for example in an act() method), do:  
+    long currentTime = System.currentTimeMillis();  
+    int durationMillis = (int)(currentTime - startTime);  
+    // durationMillis is in milliseconds, so you  
+    // can convert to seconds/minutes whatever:  
+    int durationSecs = durationMillis / 1000; 
+    health = totalTime - durationSecs;
     }
     
-    public void loseHealth()
-    {
-       health--; 
-    }
+    
 }
 
 
